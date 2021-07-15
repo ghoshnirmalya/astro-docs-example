c5b9c9897a91d30b39fe5aeb57b3ee9797b602d5
Nirmalya Ghosh <nirmalya.email@gmail.com>
2021-07-15 09:05:47 +0530

    chore: removes unncessary code

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,12 @@ export default {
   // dist: './dist',       // When running `astro build`, path to final static output
   // public: './public',   // A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing.
   buildOptions: {
-    // site: 'http://example.com',           // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
+    site: "https://astro-docs-example.vercel.app/",
     // sitemap: true,       // Generate sitemap (set to "false" to disable)
   },
   devOptions: {
     // port: 3000,         // The port to run the dev server on.
-    // tailwindConfig: '', // Path to tailwind.config.js if used, e.g. './tailwind.config.js'
+    tailwindConfig: "./tailwind.config.js",
   },
-  renderers: [
-    '@astrojs/renderer-preact'
-  ]
+  renderers: ["@astrojs/renderer-preact"],
 };
