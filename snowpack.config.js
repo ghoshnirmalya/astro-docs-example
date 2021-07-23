ba1f8629c333c1721e7f75ac10b100fbe6dfaa00
Nirmalya Ghosh <nirmalya.email@gmail.com>
2021-07-23 21:06:04 +0530

    feat: adds single article page

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,4 +1,7 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
   plugins: ["@snowpack/plugin-dotenv"],
+  packageOptions: {
+    knownEntrypoints: ["astro/dist/internal/__astro_slot.js"],
+  },
 };
